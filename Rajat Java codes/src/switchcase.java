bc0818b98afd7ea46f5a1e4824065a04490db334
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 02:35:37 +0530

    Switch statements

---
+++ Rajat Java codes/src/switchcase.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class switchcase {
+		public static void main(String[] args) {
+			Scanner sn=new Scanner(System.in);
+			System.out.println("enter the characters");
+			
+		char c=sn.next().charAt(0);
+		
+		   switch (c)
+		{
+		   case 'a':
+		  
+			   System.out.println("A vowel ");
+			   break;
+		   
+		   case 'e':
+		   
+			   System.out.println("A vowel ");
+			   break;
+		   
+		   case 'i':
+		   
+			   System.out.println("A vowel ");
+			   break;
+			   
+		   
+		   case 'o':
+		   
+		  System.out.println("A vowel ");
+			   break;
+		   
+		   case 'u':
+		   
+			   System.out.println("A vowel ");
+			   break;
+		   
+		   default : 
+		   
+			   System.out.println("a consonant");
+		   }
+		}
+	
+}
