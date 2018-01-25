a0ad490b6a3df3b53a4b44472f03eccb180e2828
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 01:52:53 +0530

    vowel or not

---
+++ Rajat Java codes/src/vowel.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class vowel {
+	public static void main(String[] args)
+	{
+//		Is the character is vowel or not
+			System.out.println("enter the character");
+			  Scanner sn=new Scanner(System.in);
+			  char var=sn.next().charAt(0);
+			    switch(var)
+			    {
+			  case 'a':
+				  System.out.println("vowel");
+			  break;
+			  
+			  case 'e':
+			  
+				  System.out.println("vowel");
+			  break;
+			  
+			  case 'i':
+				  
+				  System.out.println("vowel");
+			  break;
+				  
+			  case 'o':
+				  
+				  System.out.println("vowel");
+			  break;
+				  
+			  case 'u':
+				  
+				  System.out.println("vowel");
+			  break;  
+			  
+			    default:
+			    	System.out.println("consonant");
+				
+
+			    }
+	}
+	
+
+}
