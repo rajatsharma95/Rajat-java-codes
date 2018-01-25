644c5cb036e44695c25212f4718c5ec4ea4eccbf
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 03:30:19 +0530

    Pattern 4

---
+++ Rajat Java codes/src/pattern4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class pattern4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter rows");
+		
+		int rows = sc.nextInt();		
+		for (int i = 1; i <= rows; i++) 
+		{
+			for (int j = 1; j <= i; j++) 
+			{ 
+				System.out.print("*"); 
+			} 
+			
+			System.out.println(); 
+		} 
+		
+		for (int i = rows-1; i >= 1; i--)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+}
