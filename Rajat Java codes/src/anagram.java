14ae6b7ee406269bb747be12ac3e3872dbef5556
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 03:27:37 +0530

    Anagram

---
+++ Rajat Java codes/src/anagram.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class anagram {
+	public static void main(String[] args) {
+	Scanner sn =new Scanner(System.in);
+	System.out.println("enter string 1");
+	String s1= sn.nextLine();
+    System.out.println("enter string 2");
+    String s2= sn.nextLine();
+    int c=s1.length();
+    int d=s2.length();
+    	if(c==d)
+    	{
+    		 char a[]= s1.toCharArray();
+    		Arrays.sort(a);
+    		String sorted=new String(a);
+    		char b[]=s2.toCharArray();
+    		Arrays.sort(b);
+    		String sorted1=new String(b);
+    		if(sorted.equals(sorted1))  //comparing the each element of sorted and sorted 1 one by one
+    		{
+    			System.out.println("It is an anagram");
+    		}
+    		else
+    		{
+    			System.out.println("Not an anagram");
+    		}
+    				
+    	}
+}
+}
