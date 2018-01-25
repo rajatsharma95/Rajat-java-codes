848bae9567efef157cbe0c92c761d7da235a2273
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 00:40:11 +0530

    smallest no. in the array

---
+++ Rajat Java codes/src/smallest.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class smallest {
+	public static void main(String[] args)
+	{
+		 System.out.println("How many elements you want to enter");
+		 Scanner sn=new Scanner(System.in);
+		 int n=sn.nextInt();
+		 System.out.println("Enter the Elements");
+		 int arr[]=new int[n];
+		 int i,j=0,temp;
+		 for(i=0;i<n;i++)
+		 {
+			 arr[i]=sn.nextInt();
+		 }
+		 for(i=0;i<n;i++)
+		 {
+			 for(j=i+1;j<n;j++)
+			 {
+				 if(arr[i]<arr[j])
+				 {
+					 temp=arr[i]; 
+					 arr[i]=arr[j];
+					 arr[j]=temp;
+				 }
+			 }
+		 }System.out.println("smallest no. is " +arr[j-1]);
+
+}
+}
+
+
+
+
