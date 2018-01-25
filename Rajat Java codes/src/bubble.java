1b915770d18e6367c22663b1de270be1cb034e86
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 02:32:31 +0530

    Bubble sort

---
+++ Rajat Java codes/src/bubble.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class bubble {
+	  public static void main(String []args) {
+	    int n, c, d, swap;
+	    Scanner in = new Scanner(System.in);
+	 
+	    System.out.println("How many elements do you want to print");
+	    n = in.nextInt();
+	 
+	    int array[] = new int[n];
+	 
+	    System.out.println("Enter " + n + " integers");
+	 
+	    for (c = 0; c < n; c++) 
+	      array[c] = in.nextInt();
+	 
+	    for (c = 0; c < ( n - 1 ); c++) {
+	      for (d = 0; d < n - c - 1; d++) {
+	        if (array[d] > array[d+1]) 
+	        {
+	          swap       = array[d];
+	          array[d]   = array[d+1];
+	          array[d+1] = swap;
+	        }
+	      }
+	    }
+	 
+	    System.out.println("Sorted list of numbers");
+	 
+	    for (c = 0; c < n; c++) 
+	      System.out.println(array[c]);
+	  }
+	
+}
