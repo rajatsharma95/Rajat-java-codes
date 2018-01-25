f50c09002df6d8cffa626a4681533e770bfe80d2
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 02:28:45 +0530

    Number is even or odd

---
+++ Rajat Java codes/src/evenodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class evenodd {
+	public static void main(String[] args) {
+		
+		
+		 System.out.println("enter any number");
+		  Scanner sn=new Scanner(System.in);
+		  int var=sn.nextInt();
+		  if(var%2==0)
+		  {
+			  System.out.println("number is even");
+		  }
+		  else
+		  {
+			  System.out.println("number is odd");
+		  }
+}
+}
