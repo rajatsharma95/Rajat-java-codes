f792806b6bbe858d38be479d0bc33a608ac63700
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-26 10:43:44 +0530

    Linear search

--- Rajat Java codes/src/linear.java
+++ Rajat Java codes/src/linear.java
@@ -20,13 +20,13 @@ public class linear {
 		 
 		    for (c = 0; c < n; c++)
 		    {
-		      if (array[c] == search)     /* Searching for the if that element is present */
+		      if (array[c] == search)     /* Searching for the element if that is present */
 		      {
 		         System.out.println(search + " is present at location " + (c + 1) + ".");
 		          break;
 		      }
 		   }
-		   if (c == n)  /*if the element to search is not present */
+		   if (c == n)  /*if the element which we are searching is not present */
 		      System.out.println(search + " is not present in array.");
 		  }
 		}
