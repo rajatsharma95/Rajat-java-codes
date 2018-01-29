578e4f978ce60c67c368a34cebec14d0f0dd6cb8
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-29 08:31:08 +0530

    string concatenation

---
+++ Rajat Java codes/src/string.java
@@ -0,0 +1,12 @@
+public class string {
+//we don't use (import java.util.*) because here we donot require the scanner sn=new scanner(System.in)
+	public static void main(String[] args) {
+		String s1="I am a classy boy ";
+		String s2="You are not classy girl";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+	
+
+	}
+
+}
