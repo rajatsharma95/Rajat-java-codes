f271fa4df762a2fd5dc39d1dd7765c290b001c1b
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-29 21:33:22 +0530

    string reverse

--- Rajat Java codes/src/rev.java
+++ Rajat Java codes/src/rev.java
@@ -1,7 +1,7 @@
 
 public class rev {
 	public static void main(String[] args) {
-	StringBuffer s1=new StringBuffer("I am a classy boy");//In place of String we are using stringbuffer because reverse function is not defined in the String class, but it is defined in the Stringbuffer 
+	StringBuffer s1=new StringBuffer("I am a classy boy");//In place of String we are using stringbuffer because reverse function is not defined in the String class, but it is defined in the Stringbuffer class
 	/*string buffer-
 	Java provides the StringBuffer and String classes, and the String class is used to manipulate character strings that cannot be changed. Simply stated, objects of type String are read only and immutable. The StringBuffer class is used to represent characters that can be modified.*/
 	String s2="You are not classy girl";
@@ -9,4 +9,5 @@ public class rev {
 	
 	}
 }
+
 
