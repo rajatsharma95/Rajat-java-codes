6e644bea8d1a3a9a8366913a3f13f3e0687e2d5b
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-01-29 09:49:54 +0530

    string reverse

---
+++ Rajat Java codes/src/rev.java
@@ -0,0 +1,11 @@
+
+public class rev {
+	public static void main(String[] args) {
+	StringBuffer s1=new StringBuffer("I am a classy boy"); /*string buffer-
+	Java provides the StringBuffer and String classes, and the String class is used to manipulate character strings that cannot be changed. Simply stated, objects of type String are read only and immutable. The StringBuffer class is used to represent characters that can be modified.*/
+	String s2="You are not classy girl";
+	System.out.println(s1.reverse());
+	
+	}
+}
+
