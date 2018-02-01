1a7af91d0b08e62a39c40c6c78d0558fa8637acb
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-02-01 16:08:52 +0530

    using This reference to print the numbers and their sum

---
+++ Rajat Java codes/src/Demo.java
@@ -0,0 +1,46 @@
+// Demo for this keyword
+
+public class Demo {
+	int x;
+	int a;
+	public Demo() {
+	
+		System.out.println("Default Constructor : "+x);
+	}
+	
+	public Demo(int x) {
+		this.x = x;
+		
+	}
+	
+	public Demo(int x,int a) {
+		this(x);
+		this.a=a;
+		
+	}
+	
+	public void show()
+	{
+		System.out.println("a = "+a);
+		System.out.println("x = "+x);
+	}
+	
+	public void Add()
+	{
+		int res = a+x;
+		System.out.println("res = "+res);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Demo obj;
+	/*obj = new Demo();
+	obj.show();
+	obj.Add();*/
+	obj = new Demo(12,90);
+	obj.show();
+	obj.Add();
+	
+	}
+	}
