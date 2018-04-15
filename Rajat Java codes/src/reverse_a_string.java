b1484c67837ed1520abfd502a32f86c786f56a94
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-04-15 13:53:50 +0530

    Reverse the string

--- Rajat Java codes/src/reverse_a_string.java
+++ Rajat Java codes/src/reverse_a_string.java
@@ -5,12 +5,16 @@ public class reverse_a_string {
 		
 		String a="ABCD";
 		
-		StringBuilder sb=new StringBuilder();
-		sb=sb.append(a); 
-		sb=sb.reverse();
+		StringBuilder sb=new StringBuilder(); //here we are creating the empty string Builder with the initial capacity of 16
+		                                      //refer to https://www.javatpoint.com/StringBuilder-class
+
+		sb=sb.append(a); // here the Stringbuilder sb will concatenate the String a as ABCD
+		sb=sb.reverse(); //now here the Stringbuilder sb will reverse the string
 		System.out.println("Reverse of a string is  " +sb);
 		
-
 	}
 
 }
+
+//String is immutable means we cannot change its value, therefore we use Stringbuilder because it makes the 
+//string mutable i.e we can make any changes to the String
