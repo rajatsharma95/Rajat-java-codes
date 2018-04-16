d03005e5612def8ebe2869f8a6c3c590a7fb76ce
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-04-17 01:55:48 +0530

    string

--- Rajat Java codes/src/string.java
+++ Rajat Java codes/src/string.java
@@ -1,10 +1,38 @@
 public class string {
 //we don't use (import java.util.*) because here we donot require the scanner sn=new scanner(System.in)
 	public static void main(String[] args) {
-		String s1="I am a classy boy ";
+	
+		//1st
+		/*String s1="I am a classy boy ";
 		String s2="You are not classy girl";
 		String s3=s1.concat(s2);
-		System.out.println(s3);
+		System.out.println(s3);*/
+		
+		
+		
+		//2nd
+		/*String s="welcoMetohmt";
+		String s1="WELCOMETOHMT";
+		char c=s.charAt(1);//printing the character at index 1
+		if(s.equalsIgnoreCase(s1)) // this will compare the string s and s1 but donot compare their Uppercase & lowercase
+		{
+			System.out.println("God is great");
+		}
+		else
+		{
+			System.out.println("God is not great");
+		}*/
+		
+		
+		//3rd
+		
+		String s="welcoMetohmt";
+		char c[]=s.toCharArray(); //Changing string s into the character array
+		c[0] = Character.toUpperCase(c[0]);  //uppercasing the 0th index
+		c[5]=Character.toLowerCase(c[5]);    //lowercasing the 5th index
+		String str=new String(c);         //converting char array into string
+		System.out.println(str);
+		
 	
 
 	}
