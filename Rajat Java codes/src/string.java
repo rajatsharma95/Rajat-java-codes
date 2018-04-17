ca6a9fc164bfac41744afc0c5c6ca5e70c3d1ec7
Rajat Sharma <Rajat Sharma@Rajatsharma>
2018-04-17 15:52:28 +0530

    string

--- Rajat Java codes/src/string.java
+++ Rajat Java codes/src/string.java
@@ -7,14 +7,20 @@ public class string {
 		
 		
 		//1st
-		String s1="I am a classy boy ";
-		String s2="You are not classy girl";
-		String s3=s1.concat(s2);
-		System.out.println(s3);
-		
-		
+		System.out.println("Enter the name ");
+		Scanner sn=new Scanner(System.in);
+		String s=sn.next();
+		String d=s.substring(2, 5);
+		System.out.println(d);
 		
 		//2nd
+/*		String s1="I am a classy boy ";
+		String s2="You are not classy girl";
+		String s3=s1.concat(s2);
+		System.out.println(s3);*/
+		
+		
+		//3rd
 		/*String s="welcoMetohmt";
 		String s1="WELCOMETOHMT";
 		char c=s.charAt(1);//printing the character at index 1
@@ -28,8 +34,8 @@ public class string {
 		}*/
 		
 		
-		//3rd
 		
+		//4th
 		/*String s="welcoMetohmt";
 		char c[]=s.toCharArray(); //Changing string s into the character array
 		c[0] = Character.toUpperCase(c[0]);  //uppercasing the 0th index
@@ -39,7 +45,7 @@ public class string {
 		
 	*/
 		
-		//4th
+		//5th
 		/*
 		String s="Rajat";
 		char a[]=s.toCharArray();
